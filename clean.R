a78bc0fc9dd5e5aea7f424e54035287a8e2dc929
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:51:10 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,10 +1,10 @@
 
-clean_data <- function(dfStates){                                   #Creating a new function which cleans the dataframe 'dfStates'
-  dfStates <- dfStates[-1,-1:-4]
-  lastIndex <- nrow(dfStates)
-  dfStates <- dfStates[-lastIndex,]
-  colnames(dfStates) <- c("stateName", "population", "popOver18", "percentOver18")
-  return(dfStates)
+clean_data <- function(states){                                   #Creating a new function which cleans the dataframe 'dfStates'
+  states <- states[-1,-1:-4]
+  lastIndex <- nrow(states)
+  states <- states[-lastIndex,]
+  colnames(states) <- c("stateName", "population", "popOver18", "percentOver18")
+  return(states)
 }
 states <- clean_data() 
 View(states)
