dfab82f396e8d019a0ef573520bf66913edf3ceb
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 15:24:24 -0400

    Saving map2.R

--- map2.R
+++ map2.R
@@ -6,7 +6,7 @@ library("ggmap")
 #turning data from the maps package into a data frame suitable for plotting with ggplot2
 us <- map_data("state")
 
-#initializing  a ggplot object and passing mergeDf as the input data with map ID as stateName
+#initializing  a ggplot object and passing mergeDataframe as the input data with map ID as stateName
 map2 <- ggplot(mergeDataframe, aes(map_id = stateName))
 #creating a map visualization based on murder rate of each state
 map2 <- map2 + geom_map(map = us, aes(fill=Murder))
