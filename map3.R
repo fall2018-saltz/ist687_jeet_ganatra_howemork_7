3235542867b1f51d9926cacc7411fbb8334ef7a0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 14:53:29 -0400

    Saving map3.R

--- map3.R
+++ map3.R
@@ -9,7 +9,9 @@ us <- map_data("state")
 map3 <- ggplot(mergeDataframe, aes(map_id = stateName))
 #creating a map visualization
 map3 <- map3 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "white", high = "red")             
-#defining the x and y axes values of the map and coord_map() handles the distortion and aspect ratio of the map
+#defining the x and y axes values of the map and coord_map() handles the distortion and 
+#aspect ratio of the map
 map3 <- map3 + expand_limits(x = us$long, y = us$lat) + coord_map()
+ #geom_point() plots points at the center of every statesize attribute controls the size of the points
 map3 <- map3 + geom_point(data = mergeDataframe, mapping = aes(x = x, y = y, size=population))
 map3
