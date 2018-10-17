af108b90a515873939b003b33d5b671ecbdaa571
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 14:08:53 -0400

    Saving mergingDataframe.R

--- mergingDataframe.R
+++ mergingDataframe.R
@@ -18,9 +18,10 @@ stateCenter <- state.center
 #merging above three datasets to form a dataframe
 otherDf <- data.frame(stateName, stateArea, stateCenter)     
 
-#merging columns of arrests and states dataset with reference to stateName and pasting into a new dataset
+#merging columns of arrests and states dataset with reference to stateName and merging 
+#into a new dataset
 mergeDataframe <- merge(mergeDataframe, otherDf, by = "stateName")     
 
-
-mergeDataframe$stateName <- tolower(mergeDataframe$stateName)     #converting all state names to lower case because R cannot process capital letters
+#converting all state names to lower case because R cannot process capital letters
+mergeDataframe$stateName <- tolower(mergeDataframe$stateName)     
 mergeDataframe
