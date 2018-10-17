36da227e49351038a4eab8ae33c127b3761c0b84
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 15:23:11 -0400

    Saving map3.R

--- map3.R
+++ map3.R
@@ -5,7 +5,7 @@ library("ggmap")
 
 #turning data from the maps package into a data frame suitable for plotting with ggplot2
 us <- map_data("state")
-#initializing  a ggplot object and passing mergeDf as the input data with map ID as stateName
+#initializing  a ggplot object and passing mergeDataframe as the input data with map ID as stateName
 map3 <- ggplot(mergeDataframe, aes(map_id = stateName))
 #creating a map visualization
 map3 <- map3 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "white", high = "red")             
