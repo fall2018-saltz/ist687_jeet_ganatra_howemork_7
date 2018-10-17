2efb6261f0fc9e9dbf3f793c71285036537c56fb
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 12:17:25 -0400

    Saving mergingDataframe.R

--- mergingDataframe.R
+++ mergingDataframe.R
@@ -1,1 +1,10 @@
 
+#copying USArrests into a local variable
+arrests <- USArrests
+
+#creating a column name for rows having states using rownames()
+rownames(clean_data) <- as.vector(clean_data[,"stateName"])
+
+#merging 2 dataframes with attributes from both dataframes
+mergeDataframe <- merge(clean_data, arrests, by = "row.names")
+mergeDataframe
