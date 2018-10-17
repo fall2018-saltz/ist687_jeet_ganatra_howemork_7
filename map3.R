2697597f476fd79b3471ea41d8b963c147457c74
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 14:53:41 -0400

    Saving map3.R

--- map3.R
+++ map3.R
@@ -12,6 +12,7 @@ map3 <- map3 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "w
 #defining the x and y axes values of the map and coord_map() handles the distortion and 
 #aspect ratio of the map
 map3 <- map3 + expand_limits(x = us$long, y = us$lat) + coord_map()
- #geom_point() plots points at the center of every statesize attribute controls the size of the points
+#geom_point() plots points at the center of every state
+#size attribute controls the size of the points
 map3 <- map3 + geom_point(data = mergeDataframe, mapping = aes(x = x, y = y, size=population))
 map3
