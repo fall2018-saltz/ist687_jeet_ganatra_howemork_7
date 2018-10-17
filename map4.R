3e123d667c6afe16f8f9cc3a76e6f55853ed291e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 15:14:48 -0400

    Saving map4.R

--- map4.R
+++ map4.R
@@ -13,8 +13,10 @@ latlon
 #initializing  a ggplot object and passing mergeDf as the input data with map ID as stateName
 #and creating a map visualization
 map4 <- ggplot(mergeDataframe,aes(map_id = stateName)) + geom_map(map= us, aes(fill = Murder))
-#defining the x and y axes values of the map and #coord_map() handles the distortion and aspect ratio of the map
-map4 <- map4 + expand_limits(x= mergeDf$x,  y= mergeDf$y) + coord_map() 
+#defining the x and y axes values of the map and #coord_map() handles the distortion and 
+#aspect ratio of the map
+map4 <- map4 + expand_limits(x= x,  y= y) + coord_map() 
+#ggtitle() gives a title to the map
 map4 <- map4 + ggtitle("Zoomed map of US") 
 #Observations not in range will be dropped completely
 map4 <- map4 + xlim(latlon$lon-10,latlon$lon+10) + ylim(latlon$lat-10, latlon$lat+10)     
