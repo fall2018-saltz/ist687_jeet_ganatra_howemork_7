52baea7d6fcc174e8141e42293ad9f39608dfd2e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 15:11:57 -0400

    Saving map4.R

--- map4.R
+++ map4.R
@@ -1,5 +1,12 @@
 
-latlon <- geocode("new york city, ny", source = "dsk")     #getting the latitude and longitude of an address. source attribute hold the source of the geocodes
+#importing library for ggplot2 and ggmap
+library("ggplot2")
+library("ggmap")
+
+#turning data from the maps package into a data frame suitable for plotting with ggplot2
+us <- map_data("state")
+
+latlon <- geocode("new york city, ny", source = "dsk")     
 latlon
 mapArea3 <- ggplot(mergeDf,aes(map_id = stateName)) + geom_map(map= us, aes(fill = Murder)) + expand_limits(x= mergeDf$x,  y= mergeDf$y) + coord_map() + ggtitle("Zoomed map of US") 
 mapArea3 <- mapArea3 + xlim(latlon$lon-10,latlon$lon+10) + ylim(latlon$lat-10, latlon$lat+10)     #Observations not in range will be dropped completely
