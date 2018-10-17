9867653242da3c40ec05a8bcc39d6ece87eb0f51
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 15:18:31 -0400

    Saving map4.R

--- map4.R
+++ map4.R
@@ -22,10 +22,14 @@ map4 <- map4 + ggtitle("Zoomed map of US")
 map4 <- map4 + xlim(latlon$lon-10,latlon$lon+10) + ylim(latlon$lat-10, latlon$lat+10)     
 map4
 
+#importing library for ggplot2 and ggmap
 library("ggplot2")
 library("ggmap")
+
+#turning data from the maps package into a data frame suitable for plotting with ggplot2
 us <- map_data("state")
-options(scipen=999)
+#getting the latitude and longitude of an address
+#source attribute hold the source of the geocodes
 latlon <- geocode("new york city, ny",source="dsk")
 y1 <- latlon$lat-10
 x1 <- latlon$lon-10
