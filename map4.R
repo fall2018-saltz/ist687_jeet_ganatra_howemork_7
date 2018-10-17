b7479e300cb808931008f85a1f4c097aae1edc7d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 15:18:45 -0400

    Saving map4.R

--- map4.R
+++ map4.R
@@ -35,6 +35,7 @@ y1 <- latlon$lat-10
 x1 <- latlon$lon-10
 x2 <- latlon$lon+10
 y2 <- latlon$lat+10
+#initializing  a ggplot object and passing mergeDataframe as the input data with map ID as stateName
 map4 <- ggplot(clean_data_merged, aes(map_id = stateName))
 map4 <- map4 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "white", high = "red") + xlim(c(x1,x2)) + ylim(c(y1,y2))        
 map4 <- map4 + expand_limits(x = us$long, y = us$lat) + coord_map()
