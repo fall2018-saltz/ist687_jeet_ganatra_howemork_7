e07e8f8c5c23535ea3863a20da0708a811cdaee8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 12:24:53 -0400

    Saving mergingDataframe.R

--- mergingDataframe.R
+++ mergingDataframe.R
@@ -9,10 +9,11 @@ rownames(clean_data) <- as.vector(clean_data[,"stateName"])
 mergeDataframe <- merge(clean_data, arrests, by = "row.names")
 mergeDataframe
 
-#Adding the area of each state, and the center of each state, to the merged dataframe, using the ‘state.center’, ‘state.center’ and ‘state.name’ vectors
-
+#Adding the area of each state, and the center of each state, to the merged dataframe, 
+#using the ‘state.center’, ‘state.center’ and ‘state.name’ vectors
 statecenterx <- state.center$x
 statecentery <- state.center$y
+#the cbind() adds the new columns to the existing dataset
 mergeDataframe <- cbind(mergeDataframe,state.area)
 mergeDataframe <- cbind(mergeDataframe,statecenterx)
 mergeDataframe <- cbind(mergeDataframe,statecentery)
