2af28001f73454db6dce64405eb8971a0ca8bc29
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 15:13:38 -0400

    Saving map4.R

--- map4.R
+++ map4.R
@@ -10,9 +10,10 @@ us <- map_data("state")
 #source attribute hold the source of the geocodes
 latlon <- geocode("new york city, ny", source = "dsk")     
 latlon
-
+#initializing  a ggplot object and passing mergeDf as the input data with map ID as stateName
 map4 <- ggplot(mergeDataframe,aes(map_id = stateName)) + geom_map(map= us, aes(fill = Murder)) 
 map4 <- map4 + expand_limits(x= mergeDf$x,  y= mergeDf$y) + coord_map() 
 map4 <- map4 + ggtitle("Zoomed map of US") 
+#Observations not in range will be dropped completely
 map4 <- map4 + xlim(latlon$lon-10,latlon$lon+10) + ylim(latlon$lat-10, latlon$lat+10)     
-mapArea3
+map4
