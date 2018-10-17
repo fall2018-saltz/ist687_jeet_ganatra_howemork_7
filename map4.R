7715147f2421b753844ee6c7388daf38da90ebe6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 15:05:53 -0400

    Saving map4.R

--- map4.R
+++ map4.R
@@ -8,6 +8,7 @@ library("ggmap")
 latlon <- geocode("new york city, ny", source = "dsk")     
 latlon
 #initializing  a ggplot object and passing mergeDf as the input data with map ID as stateName
+#and creating a map visualization
 map4 <- ggplot(mergeDataframe,aes(map_id = stateName)) + geom_map(map= us, aes(fill = Murder)) 
 #defining the x and y axes values of the map and coord_map() handles the distortion and 
 #aspect ratio of the map
