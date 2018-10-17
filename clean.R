339ee46950be68e01524cec079a0d18d445820e9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:18:29 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,15 +1,28 @@
 
-clean_data <- function(states){
-# removes the first row and the last row, with state = USA and state = Puerto rico commonwealth
-# and the first four columns SUMLEV REGION DIVISION STATE
-states <- states[,-c(1:4)]
-states <- states[c(-1,-53),]
+#clean_data <- as.data.frame(raw_data)
+#names(clean_data)
+func1 <- function(states){
 
-#Using colnames() to replace the names of the columns in the dataframe.
-colnames(states)[c(1,2,3,4)] <- c("stateName", "population", "popOver18", "percentOver18")
-#returns the clean dataframe
-  return(states)
+  # removes the first row and the last row, with state = USA and state = Puerto rico commonwealth 
+  # and the first four columns SUMLEV	REGION	DIVISION	STATE
+
+  dfStates <- states[,c(-1,-2,-3,-4)]
+  dfStates <- dfStates[c(-1,-10,-53),] 
+
+  # renaming the remaining columns
+  # the colnames function is used to rename columns and the rownames function is used for rows
+
+  colnames(dfStates)[c(1,2,3,4)] <- c("stateName", "population", "popOver18", "percentOver18")
+
+  #returns the clean dataframe
+  return(dfStates)
 }
 
-clean_data <- clean_data(raw_data)
-str(clean_data)
+clean_data <- func1(raw_data)
+
+# saves the state names as row names
+rownames(clean_data) <- clean_data$stateName
+
+# convert state names to lowercase
+
+clean_data$stateName <- tolower(clean_data$stateName)
