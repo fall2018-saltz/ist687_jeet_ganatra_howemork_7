d551dfbc8a0d680bce98b3fd244fa8ca5c44c6fc
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 14:23:01 -0400

    Saving map2.R

--- map2.R
+++ map2.R
@@ -10,6 +10,9 @@ us <- map_data("state")
 map2 <- ggplot(mergeDataframe, aes(map_id = stateName))
 #creating a map visualization based on murder rate of each state
 map2 <- map2 + geom_map(map = us, aes(fill=Murder))
+#defining the x and y axes values of the map
 map2 <- map2 + expand_limits(x = mergeDataframe$x, y= mergeDataframe$y)
+#coord_map() handles the distortion and aspect ratio of the map
+#ggtitle() gives a title to the map
 map2 <- map2 + coord_map() + ggtitle("Map of US based on murder rate of states")
 map2
