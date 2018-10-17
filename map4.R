35606e4b9c7effb353f6e16180e33e8cffd3326a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 15:21:28 -0400

    Saving map4.R

--- map4.R
+++ map4.R
@@ -5,29 +5,6 @@ library("ggmap")
 
 #turning data from the maps package into a data frame suitable for plotting with ggplot2
 us <- map_data("state")
-
-#getting the latitude and longitude of an address
-#source attribute hold the source of the geocodes
-latlon <- geocode("new york city, ny", source = "dsk")     
-latlon
-#initializing  a ggplot object and passing mergeDf as the input data with map ID as stateName
-#and creating a map visualization
-map4 <- ggplot(mergeDataframe,aes(map_id = stateName)) + geom_map(map= us, aes(fill = Murder))
-#defining the x and y axes values of the map and #coord_map() handles the distortion and 
-#aspect ratio of the map
-map4 <- map4 + expand_limits(x= x,  y= y) + coord_map() 
-#ggtitle() gives a title to the map
-map4 <- map4 + ggtitle("Zoomed map of US") 
-#Observations not in range will be dropped completely
-map4 <- map4 + xlim(latlon$lon-10,latlon$lon+10) + ylim(latlon$lat-10, latlon$lat+10)     
-map4
-
-#importing library for ggplot2 and ggmap
-library("ggplot2")
-library("ggmap")
-
-#turning data from the maps package into a data frame suitable for plotting with ggplot2
-us <- map_data("state")
 #getting the latitude and longitude of an address
 #source attribute hold the source of the geocodes
 latlon <- geocode("new york city, ny",source="dsk")
@@ -38,7 +15,8 @@ y2 <- latlon$lat+10
 #initializing  a ggplot object and passing mergeDataframe as the input data with map ID as stateName
 map4 <- ggplot(mergeDataframe, aes(map_id = stateName))
 #and creating a map visualization
-map4 <- map4 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "white", high = "red") + xlim(c(x1,x2)) + ylim(c(y1,y2))        
+map4 <- map4 + geom_map(map = us,aes(fill=Murder))
+map4 <- map4 + scale_fill_gradient(low = "white", high = "red") + xlim(c(x1,x2)) + ylim(c(y1,y2))        
 #defining the x and y axes values of the map and #coord_map() handles the distortion and 
 #aspect ratio of the map
 map4 <- map4 + expand_limits(x = us$long, y = us$lat) + coord_map()
