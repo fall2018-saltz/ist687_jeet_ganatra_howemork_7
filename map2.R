106b20aa35b98ae8ae40cb70ebea203bb3c60afa
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 14:21:19 -0400

    Saving map2.R

--- map2.R
+++ map2.R
@@ -2,6 +2,8 @@
 #importing library for ggplot2 and ggmap
 library("ggplot2")
 library("ggmap")
+
+#turning data from the maps package into a data frame suitable for plotting with ggplot2
 us <- map_data("state")
 map2 <- ggplot(mergeDataframe, aes(map_id = stateName))
 map2 <- map2 + geom_map(map = us, aes(fill=Murder))
