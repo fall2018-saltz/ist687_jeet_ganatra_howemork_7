99f22d9893afb8b18bbd54ec9ee40a3f91e38cb1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 15:20:58 -0400

    Saving map4.R

--- map4.R
+++ map4.R
@@ -36,9 +36,11 @@ x1 <- latlon$lon-10
 x2 <- latlon$lon+10
 y2 <- latlon$lat+10
 #initializing  a ggplot object and passing mergeDataframe as the input data with map ID as stateName
-map4 <- ggplot(clean_data_merged, aes(map_id = stateName))
+map4 <- ggplot(mergeDataframe, aes(map_id = stateName))
 #and creating a map visualization
 map4 <- map4 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "white", high = "red") + xlim(c(x1,x2)) + ylim(c(y1,y2))        
+#defining the x and y axes values of the map and #coord_map() handles the distortion and 
+#aspect ratio of the map
 map4 <- map4 + expand_limits(x = us$long, y = us$lat) + coord_map()
-map4 <- map4 + geom_point(data = clean_data_merged, mapping = aes(x = statecenterx, y = statecentery, size=population))
+map4 <- map4 + geom_point(data = mergeDataframe, mapping = aes(x = x, y = y, size=population))
 map4
