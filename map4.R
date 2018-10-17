1caeadd60da7bfae9aacf80c5aeacfcc04cb9ec2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 15:03:22 -0400

    Saving map4.R

--- map4.R
+++ map4.R
@@ -7,6 +7,7 @@ library("ggmap")
 #source attribute hold the source of the geocodes
 latlon <- geocode("new york city, ny", source = "dsk")     
 latlon
-map4 <- ggplot(mergeDataframe,aes(map_id = stateName)) + geom_map(map= us, aes(fill = Murder)) + expand_limits(x= mergeDf$x,  y= mergeDf$y) + coord_map() + ggtitle("Zoomed map of US") 
-mapArea3 <- mapArea3 + xlim(latlon$lon-10,latlon$lon+10) + ylim(latlon$lat-10, latlon$lat+10)     #Observations not in range will be dropped completely
+map4 <- ggplot(mergeDataframe,aes(map_id = stateName)) + geom_map(map= us, aes(fill = Murder)) 
+map4 <- map4 + expand_limits(x= x,  y= y) + coord_map() + ggtitle("Zoomed map of US") 
+map4 <- map4 + xlim(latlon$lon-10,latlon$lon+10) + ylim(latlon$lat-10, latlon$lat+10)     
 mapArea3
