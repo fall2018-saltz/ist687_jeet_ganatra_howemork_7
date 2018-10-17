b13e161ac172dd769792dd7372cd59cea58306d4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 15:05:37 -0400

    Saving map4.R

--- map4.R
+++ map4.R
@@ -7,8 +7,10 @@ library("ggmap")
 #source attribute hold the source of the geocodes
 latlon <- geocode("new york city, ny", source = "dsk")     
 latlon
+#initializing  a ggplot object and passing mergeDf as the input data with map ID as stateName
 map4 <- ggplot(mergeDataframe,aes(map_id = stateName)) + geom_map(map= us, aes(fill = Murder)) 
-#defining the x and y axes values of the map and coord_map() handles the distortion and aspect ratio of the map
+#defining the x and y axes values of the map and coord_map() handles the distortion and 
+#aspect ratio of the map
 #ggtitle() gives a title to the map
 map4 <- map4 + expand_limits(x= x,  y= y) + coord_map() + ggtitle("Zoomed map of US") 
 #Observations not in range will be dropped completely
