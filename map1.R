3e067e870e0fe1a76cf44ed2c6b95a4f5941896b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:05:49 -0400

    Saving map1.R

--- map1.R
+++ map1.R
@@ -4,7 +4,6 @@ library("ggplot2")
 library("ggmap")
 #turning data from the maps package into a data frame suitable for plotting with ggplot2
 area_map <- map_data("state")
-options(scipen=999)
 #using ggplot() to create a map and geom_map() to assign characteristics for the map and
 #expand_limits() to ensure limits include a single value for all plots
 map1 <- ggplot(mergeDataframe, aes(map_id = stateName))
