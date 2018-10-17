1873c4b3b6f392e9c279421164a89ef6805362b4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 15:08:24 -0400

    Saving map4.R

--- map4.R
+++ map4.R
@@ -1,22 +1,15 @@
 
-#importing library for ggplot2 and ggmap
 library("ggplot2")
 library("ggmap")
+us <- map_data("state")
 
-#turning data from the maps package into a data frame suitable for plotting with ggplot2
-us <- map_data("state") 
-
-#getting the latitude and longitude of an address
-#source attribute hold the source of the geocodes
-latlon <- geocode("new york city, ny", source = "dsk")     
-latlon
-#initializing  a ggplot object and passing mergeDf as the input data with map ID as stateName
-#and creating a map visualization
-map4 <- ggplot(mergeDataframe,aes(map_id = stateName)) + geom_map(map= us, aes(fill = Murder)) 
-#defining the x and y axes values of the map and coord_map() handles the distortion and 
-#aspect ratio of the map
-#ggtitle() gives a title to the map
-map4 <- map4 + expand_limits(x= x,  y= y) + coord_map() + ggtitle("Zoomed map of US") 
-#Observations not in range will be dropped completely
-map4 <- map4 + xlim(latlon$lon-10,latlon$lon+10) + ylim(latlon$lat-10, latlon$lat+10)     
+latlon <- geocode("new york city, ny",source="dsk")
+y1 <- latlon$lat-10
+x1 <- latlon$lon-10
+x2 <- latlon$lon+10
+y2 <- latlon$lat+10
+map4 <- ggplot(clean_data_merged, aes(map_id = stateName))
+map4 <- map4 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "white", high = "red") + xlim(c(x1,x2)) + ylim(c(y1,y2))        
+map4 <- map4 + expand_limits(x = us$long, y = us$lat) + coord_map()
+map4 <- map4 + geom_point(data = clean_data_merged, mapping = aes(x = statecenterx, y = statecentery, size=population))
 map4
