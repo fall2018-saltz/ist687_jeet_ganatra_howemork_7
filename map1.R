8a317fa2e09a957331f69f7bba5f954d2191384e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 14:15:17 -0400

    Saving map1.R

--- map1.R
+++ map1.R
@@ -2,9 +2,11 @@
 #importing library for ggplot2 and ggmap
 library("ggplot2")
 library("ggmap")
+
+#turning data from the maps package into a data frame suitable for plotting with ggplot2
 us <- map_data("state")     
-View(us)
-mapArea <- ggplot(mergeDf, aes(map_id = stateName))     #initializing  a ggplot object and passing mergeDf as the input data with map ID as stateName.
+
+mapArea <- ggplot(mergeDf, aes(map_id = stateName))     
 mapArea <- mapArea + geom_map(map = us, aes(fill= stateArea))     #creating a map visualization
 mapArea <- mapArea + expand_limits(x = mergeDf$x, y= mergeDf$y)     #defining the x and y axes values of the map
 mapArea <- mapArea + coord_map() + ggtitle("Map of US based on state area")     #coord_map() handles the distortion and aspect ratio of the map. ggtitle() gives a title to the map
