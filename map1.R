689534551fe9d25495dfe49a8c1f50bf456f6dee
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:00:26 -0400

    Saving map1.R

--- map1.R
+++ map1.R
@@ -1,9 +1,10 @@
 
+#importing library for ggplot2 and ggmap
 library("ggplot2")
 library("ggmap")
-us <- map_data("state")
+area_map <- map_data("state")
 options(scipen=999)
-map1 <- ggplot(clean_data_merged, aes(map_id = stateName))
-map1 <- map1 + geom_map(map = us ,aes(fill= state.area))
+map1 <- ggplot(mergeDataframe, aes(map_id = stateName))
+map1 <- map1 + geom_map(map = area_map ,aes(fill= state.area))
 map1 <- map1 + expand_limits(x = us$long, y = us$lat) + coord_map()
 map1
