f1897b5d8814cb64cc551a99c0770ba6b3f29930
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 14:49:05 -0400

    Saving map3.R

--- map3.R
+++ map3.R
@@ -1,10 +1,13 @@
 
+#importing library for ggplot2 and ggmap
 library("ggplot2")
 library("ggmap")
+
+#turning data from the maps package into a data frame suitable for plotting with ggplot2
 us <- map_data("state")
-options(scipen=999)
-map3 <- ggplot(clean_data_merged, aes(map_id = stateName))
+
+map3 <- ggplot(mergeDataframe, aes(map_id = stateName))
 map3 <- map3 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "white", high = "red")             
 map3 <- map3 + expand_limits(x = us$long, y = us$lat) + coord_map()
-map3 <- map3 + geom_point(data = clean_data_merged, mapping = aes(x = statecenterx, y = statecentery, size=population))
+map3 <- map3 + geom_point(data = mergeDataframe, mapping = aes(x = x, y = y, size=population))
 map3
