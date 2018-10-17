00dd3c4c8ef2cbc957f33befbe80620a5a7145a4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 14:19:29 -0400

    Saving map2.R

--- map2.R
+++ map2.R
@@ -1,9 +1,8 @@
 
 library("ggplot2")
 library("ggmap")
-us <- map_data("state")
-options(scipen=999)
 map2 <- ggplot(mergeDataframe, aes(map_id = stateName))
-map2 <- map2 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "black", high = "red")             
-map2 <- map2 + expand_limits(x = us$long, y = us$lat) + coord_map()
+map2 <- map2 + geom_map(map = us, aes(fill=Murder))
+map2 <- map2 + expand_limits(x = mergeDataframe$x, y= mergeDataframe$y)
+map2 <- map2 + coord_map() + ggtitle("Map of US based on murder rate of states")
 map2
